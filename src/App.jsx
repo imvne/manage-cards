56fae1538550de9d57a9ea772c3c904a3bd95d2d
imvne <imanebeaufils19@gmail.com>
2024-09-12 21:31:50 +0200

    ajout option ajouter et supprimer une card

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,30 @@ import CrossButton from './CrossButton';
 import './CrossButton.css'
 
 export default function App() {
-
-  return (
-    <>
-    <CrossButton addOrDeleteStyle={'delete'} buttonSize={'50px'}/>
-    <CrossButton addOrDeleteStyle={'add'} buttonSize={'50px'}/>
-    </>
-  )
+	const [cards, setCards] = useState([])
+	
+	const handleAddButtonClick = () => {
+		setCards([...cards, `${cards.length+1}`])
+	}
+	
+	const handleDeleteButtonClick = (cardToDelete) => {
+		setCards(cards.filter(card => card!== cardToDelete))
+	}
+	
+	return (
+	<>
+	<div className="cards-container">
+		
+		{cards.map((card) => {return (
+			<div key={card} className="card" >
+				<CrossButton className={"delete-card-button"} addOrDeleteStyle={'delete'} buttonSize={'20px'} handleOnClick={() => handleDeleteButtonClick(card)}/>
+			</div>
+		)})}
+		
+	</div>
+	<CrossButton addOrDeleteStyle={'add'} buttonSize={'50px'} handleOnClick={handleAddButtonClick}/>
+	</>
+	)
 }
 
 
