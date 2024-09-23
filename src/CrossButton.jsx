62dae68f8fabf04f358b6e5d399a8c0027db85da
imvne <imanebeaufils19@gmail.com>
2024-09-23 20:27:38 +0200

    indentation vscode

--- src/CrossButton.jsx
+++ src/CrossButton.jsx
@@ -1,8 +1,12 @@
-export default function CrossButton({className, addOrDeleteStyle, buttonSize, handleOnClick}){
+export default function CrossButton({className, addOrDeleteStyle, buttonSize, handleOnClick, onOrOff}){
 	
 	return (
 		
-		<div onClick={handleOnClick} className={`${addOrDeleteStyle}-button ${className}`} style={{width: buttonSize, height: buttonSize}}>
+		<div 
+		onClick={handleOnClick} 
+		className={`${addOrDeleteStyle}-button ${className} button-${onOrOff}`} 
+		style={{width: buttonSize, height: buttonSize}}>
+			
 			<div className="cross">
 				<div className='a'></div>
 		  		<div className='b'></div>
