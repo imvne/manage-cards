33044d256f6b2f1fa822dd1b5e0c97588798360b
imvne <imanebeaufils19@gmail.com>
2024-09-23 20:25:42 +0200

    modification du state "cardsKey" en 'cards" qui contient une valeur pour key, height, et content + ajout d'un bouton pour activer et désactiver le add button

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,41 @@ import CrossButton from './CrossButton';
 import CardsContainer from './CardsContainer';
 
 export default function App() {
-	const [cardsKeys, setCardsKeys] = useState([])
+	const [cards, setCards] = useState([]) // {key : "hbkve", heigth: "", content: ""}
+	const [addButton, setAddButton] = useState("on")
 	const cardsContainerHeight = "50vw"
 	
 	const handleAddButtonClick = () => {
-		
-		
-		setCardsKeys([...cardsKeys, `${crypto.randomUUID()}`])
+		if (addButton === "on"){
+			setCards(previousCards => [...previousCards, {key : crypto.randomUUID(), height: 0, content: ""}])
+			
+		}
 	}
-	
 	
 	return (
 	<>
 		<CardsContainer
 			cardsContainerHeight={cardsContainerHeight}
 			className={"cards-container"}
-			cardsKeys={cardsKeys}
-			setCardsKeys={setCardsKeys}
+			cards={cards}
+			setCards={setCards}
 		/>
 		
 		<CrossButton
 			className={"add-card-button"}
 			addOrDeleteStyle={'add'} 
 			buttonSize={'50px'} 
-			handleOnClick={handleAddButtonClick}
+			handleOnClick={ () => {handleAddButtonClick()}}
 		/>
+		
+		<button 
+			style={{height: "20px", width: "60px"}}
+			onClick={() => {
+				addButton === "on" ? setAddButton("off") : setAddButton("on")
+			}}
+		>
+			{addButton}
+		</button>
 	</>
 	)
 }
