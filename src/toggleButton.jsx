3276fc1e5fd70ebf79b0fd4944ef00c6ff6b673d
imvne <imanebeaufils19@gmail.com>
2024-09-27 18:17:52 +0200

    enelvé le css du fichier jsx

--- src/toggleButton.jsx
+++ src/toggleButton.jsx
@@ -3,20 +3,38 @@ import './ToggleButton.css'
 
 export default function ToggleButton({toggleOffText, toggleOnText, toggleOn, handleClick}) {	
 	
-	const toggleOnStyle = {text : {color: 'rgb(100, 100, 156)', fontWeight: '500', fontSize: '16.5px'}, button : {backgroundColor: 'rgb(159, 193, 147)'}};
-	const toggleOffStyle = {text : {color: 'rgb(137, 184, 119)', fontWeight: '500', fontSize: '16.5px'}, button : {backgroundColor: 'rgb(99, 99, 124)'}}
-	const notChosenTextStyle = {color: 'rgb(113, 113, 113)', fontWeight: '400', fontSize: '16px'};
-
 	return (
 		<div className="toggleDiv">
 			
-			<p style={toggleOn === true ? notChosenTextStyle : toggleOnStyle.text}>{toggleOffText}</p>
-			
-				<div className="toggle-rectangle"  style={toggleOn === true ? toggleOnStyle.button : toggleOffStyle.button}>
-					<div className="toggle-circle" onClick={handleClick} style={toggleOn ? {transform: 'translate(140%)'} : {transform: 'translate(0%)'}}></div>
-				</div>
+		{
+			toggleOn ? (
+				<>
+				<p className="text-not-chosen"> {toggleOffText} </p>
 				
-			<p style={toggleOn === true ? toggleOffStyle.text : notChosenTextStyle}>{toggleOnText}</p>
+				<div className={"toggle-rectangle toggle-on-rectangle"} >
+					
+						<div className="toggle-circle toggle-on-circle" onClick={handleClick}> </div>
+					
+				</div>
+					
+				<p className="right-text-chosen"> {toggleOnText} </p>
+				</>
+			) : (
+				<>
+				<p className="left-text-chosen"> {toggleOffText} </p>
+				
+				<div className={"toggle-rectangle toggle-off-rectangle"} >
+					
+						<div className="toggle-circle" onClick={handleClick}> </div>
+					
+				</div>
+					
+				<p className={"text-not-chosen"}> {toggleOnText} </p>
+				</>
+			)
+		}
+			
+			
 			
 		</div>
 	)
