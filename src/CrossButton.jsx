afd87a13971c9ac52cd8c18be01f9fc9e42081df
imvne <imanebeaufils19@gmail.com>
2024-10-07 17:12:28 +0200

    id

--- src/CrossButton.jsx
+++ src/CrossButton.jsx
@@ -1,10 +1,13 @@
-export default function CrossButton({className, addOrDeleteStyle, buttonSize, handleOnClick, addButtonOn}){
+import './CrossButton.css'
+
+export default function CrossButton({className, id, addOrDeleteStyle, buttonSize, handleOnClick, addButtonOn}){
 	
 	
 	return (
 		
 		<div 
 		onClick={handleOnClick} 
+		id={id}
 		className={`${addOrDeleteStyle}-button ${className} button-${addButtonOn ? "on" : "off"}`} 
 		style={{width: buttonSize, height: buttonSize}}>
 			
